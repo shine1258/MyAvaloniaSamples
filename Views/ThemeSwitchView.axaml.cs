3876cfed263ddfff5e4b2f4c5136f282bdd02587
shine1258 <shine2048@outlook.com>
2026-02-09 01:00:45 +0800

    添加 ThemeSwitchView

---
+++ Views/ThemeSwitchView.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
+
+namespace MyAvaloniaSamples.Views;
+
+public partial class ThemeSwitchView : UserControl
+{
+    public ThemeSwitchView()
+    {
+        InitializeComponent();
+        ThemeSwitch.IsChecked = Application.Current?.ActualThemeVariant == ThemeVariant.Dark;
+    }
+
+    private void ThemeSwitch_OnIsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        var isChecked = ThemeSwitch.IsChecked ?? false;
+        Application
+            .Current
+            ?.RequestedThemeVariant = isChecked ? ThemeVariant.Dark : ThemeVariant.Light;
+    }
+}
